7f8c4668863a83c509f2bacb1d6cbecfc49337a4
chandana <gayamchandana@gmail.com>
2025-07-25 16:55:30 +0530

    Update domain.js

--- projectwork-frontend/src/api/domain.js
+++ projectwork-frontend/src/api/domain.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8085/projectwork";
+const BASE_URL =  ${process.env.REACT_APP_API_URL};
 
 export async function fetchDomains() {
   const res = await fetch(`${BASE_URL}/domains`);
