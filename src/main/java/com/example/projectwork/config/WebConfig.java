846ed97b3e6bb5e1b9799827f3e6e0befbff68a9
chandana <gayamchandana@gmail.com>
2025-07-25 16:44:53 +0530

    Update WebConfig.java

--- src/main/java/com/example/projectwork/config/WebConfig.java
+++ src/main/java/com/example/projectwork/config/WebConfig.java
@@ -6,14 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins(frontendUrl)//  "http://localhost:5174"
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
